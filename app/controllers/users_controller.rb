e5e6cf35d66710350bdb32b95b46b6359f4bf8cf
github username <github email address>
2018-03-29 12:54:20 -0400

    working on signup post form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,14 @@ class UsersController < ApplicationController
     erb :'users/login'
   end
 
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect to '/signup'
+    else
+      @user = User.new(:username => params[:username], :password => params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/'
+    end
+  end
 end
