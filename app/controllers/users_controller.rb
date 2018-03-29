237aefe041069343d8c8960cfe1281f187261107
github username <github email address>
2018-03-29 13:32:32 -0400

    created login, signup and logout features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,23 @@ class UsersController < ApplicationController
       redirect to '/'
     end
   end
+
+  post '/login' do
+     user = User.find_by(:username => params[:username])
+     if user && user.authenticate(params[:password])
+       session[:user_id] = user.id
+       redirect "/"
+     else
+       redirect to '/signup'
+     end
+   end
+
+   get '/logout' do
+    if session[:user_id] != nil
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
