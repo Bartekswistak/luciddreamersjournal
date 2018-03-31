c88c63da3fc846064db4dcb58bc56ae3eeee2649
github username <github email address>
2018-03-31 10:59:46 -0400

    added edit form for dream entry

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,20 @@
 class EntriesController < ApplicationController
 
+  get '/entries/entries' do
+    if session[:user_id]
+      @entries = Entry.all
+      erb :'entries/entries'
+    else
+      redirect to '/login'
+    end
+  end
+
   get '/create_entry' do
-    erb :'entries/create_entry'
+    if session[:user_id]
+      erb :'entries/create_entry'
+    else
+      redirect to '/login'
+    end
   end
 
   post '/entries' do
@@ -10,13 +23,10 @@ class EntriesController < ApplicationController
     else
       user = User.find_by_id(session[:user_id])
       @entry = Entry.create(:content => params[:content], :date => params[:date], :user_id => user.id)
-      redirect to '/entries/show'
+      redirect to '/entries/entries'
     end
   end
 
-  get '/entries/show' do
-    erb :'/entries/show'
-  end
 
 
 end
