95ef94041f41decf0a264e9ab1cebc8e2f477b48
github username <github email address>
2018-03-31 10:09:12 -0400

    working on post forms and viewing entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -5,7 +5,18 @@ class EntriesController < ApplicationController
   end
 
   post '/entries' do
-    
+    if params[:content] == ""
+      redirect to '/entries/create_entry'
+    else
+      user = User.find_by_id(session[:user_id])
+      @entry = Entry.create(:content => params[:content], :date => params[:date], :user_id => user.id)
+      redirect to '/entries/show'
+    end
   end
+
+  get '/entries/show' do
+    erb :'/entries/show'
+  end
+
 
 end
