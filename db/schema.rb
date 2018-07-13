51a9f1747f2c308e2dc0c257e90f682d378d30f8
github username <github email address>
2018-07-12 20:12:24 -0400

    reverted to sqlite database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328154811) do
+ActiveRecord::Schema.define(version: 2018_03_28_154811) do
 
   create_table "entries", force: :cascade do |t|
     t.string "content"
